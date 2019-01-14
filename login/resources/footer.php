ef136d0bb435dc85dcc40f6f0f69edf33260f518
Miquel <miqueljanermudoy@outlook.com>
2019-01-14 21:22:57 +0100

    del all

--- login/resources/footer.php
+++
@@ -1,9 +0,0 @@
-
-<!--Contents-->
-			<div id="footer">
-				<a href="#" class="fa fa-facebook"></a>
-				<a href="#" class="fa fa-twitter"></a>
-				<a href="#" class="fa fa-google"></a>
-			</div>
-    </body>
-</html>
